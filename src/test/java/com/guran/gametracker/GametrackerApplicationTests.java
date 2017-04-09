d773ac80319e819e11454aa5e365a77553d15bec
Göran Paues <goran@paues.com>
2017-04-10 01:20:12 +0200

    Initial commit

---
+++ src/test/java/com/guran/gametracker/GametrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guran.gametracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GametrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
