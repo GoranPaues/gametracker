deabd4198df188b20ad18dd7c59f003cf1b79786
Göran Paues <goran@paues.com>
2017-04-23 13:31:29 +0200

    Added merge to platforms in import_games

--- src/main/resources/db/migration/R__procedure_import_games.sql
+++ src/main/resources/db/migration/R__procedure_import_games.sql
@@ -1,24 +1,20 @@
 CREATE OR REPLACE PROCEDURE gametracker.import_games AS
 
-    l_game_id        games.id%TYPE;
-    l_shelf_id       shelves.id%TYPE;
-    l_platform_id    platforms.id%TYPE;
-    l_dept_arr       json_array_t;
-    l_shelves_obj    json_object_t;
-    l_shelves_list   json_key_list;
-    l_shelf_list     json_key_list;
-    l_shelf_arr      json_array_t;
-    l_shelf_obj      json_object_t;
-    l_date_added     DATE;
-    l_boolean_char   VARCHAR2(10 CHAR);
+    l_game_id          games.id%TYPE;
+    l_shelf_id         shelves.id%TYPE;
+    l_platform_id      platforms.id%TYPE;
+    l_date_added       DATE;
+    l_shelves_obj      json_object_t;
+    l_shelves_list     json_key_list;
+    l_shelf_obj        json_object_t;
+    l_platforms_obj    json_object_t;
+    l_platforms_list   json_key_list;
 BEGIN
     FOR rc IN (
         SELECT
             *
         FROM
             grouvee_import
-        WHERE
-            ROWNUM < 2
     ) LOOP
     
         /* MERGE GAME INFO */
@@ -52,7 +48,10 @@ BEGIN
         /* MERGE SHELVES */
 
         l_shelves_obj := json_object_t(rc.shelves);
-        l_shelves_list := l_shelves_obj.get_keys;
+        l_shelves_list := nvl(
+            l_shelves_obj.get_keys,
+            json_key_list()
+        );
         FOR i IN 1..l_shelves_list.count LOOP
             BEGIN
                 SELECT
@@ -101,60 +100,57 @@ BEGIN
         END LOOP;
         /* END SHELVES */
 
-        /* MERGE SHELVES */
+        /* MERGE PLATFORMS */
 
-        l_shelves_obj := json_object_t(rc.shelves);
-        l_shelves_list := l_shelves_obj.get_keys;
-        FOR i IN 1..l_shelves_list.count LOOP
+        l_platforms_obj := json_object_t(rc.platforms);
+        l_platforms_list := nvl(
+            l_platforms_obj.get_keys,
+            json_key_list()
+        );
+        FOR i IN 1..l_platforms_list.count LOOP
             BEGIN
                 SELECT
                     id
                 INTO
-                    l_shelf_id
+                    l_platform_id
                 FROM
-                    shelves
+                    platforms
                 WHERE
-                    name = l_shelves_list(i);
+                    name = l_platforms_list(i);
 
             EXCEPTION
                 WHEN no_data_found THEN
-                    INSERT INTO shelves ( name ) VALUES ( l_shelves_list(i) ) RETURNING id INTO l_shelf_id;
+                    INSERT INTO platforms ( name ) VALUES ( l_platforms_list(i) ) RETURNING id INTO l_platform_id;
 
             END;
 
-            l_shelf_obj := l_shelves_obj.get_object(l_shelves_list(i) );
-            l_date_added := TO_DATE(
-                l_shelf_obj.get_string('date_added'),
-                'yyyy-mm-dd"T"hh24:mi:ss"Z"'
-            );
-            UPDATE shelf_list
-                SET
-                    date_added = l_date_added
+            INSERT INTO platform_list (
+                game_id,
+                platform_id
+            ) SELECT
+                l_game_id,
+                l_platform_id
+            FROM
+                dual
             WHERE
-                    game_id = l_game_id
-                AND
-                    shelf_id = l_shelf_id;
-
-            IF
-                SQL%rowcount = 0
-            THEN
-                INSERT INTO shelf_list (
-                    game_id,
-                    shelf_id,
-                    date_added
-                ) VALUES (
-                    l_game_id,
-                    l_shelf_id,
-                    l_date_added
-                );
-
-            END IF;
+                NOT
+                    EXISTS (
+                        SELECT
+                            1
+                        FROM
+                            platform_list
+                        WHERE
+                                game_id = l_game_id
+                            AND
+                                platform_id = l_platform_id
+                    );
 
         END LOOP;
-        /* END SHELVES */
+        /* END PLATFORMS */
+
+        /* END GAME INFO */
 
     END LOOP;
-    /* END GAMES */
-    
+
     COMMIT;
 END import_games;
